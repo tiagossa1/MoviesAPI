f9eedd75813892d19334fef5b503022f2a021653
tiagossa1 <tiagossa.2000@gmail.com>
2023-05-25 21:36:04 +0100

    + Removed InMemory database.

--- MoviesApi/IoC/ConfigureServices.cs
+++ MoviesApi/IoC/ConfigureServices.cs
@@ -2,10 +2,8 @@ using Application.Common.Behaviours;
 using Application.Interfaces;
 using Application.Movies.Command.CreateMovie;
 using FluentValidation;
-using Infrastructure;
 using Infrastructure.Repositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IoC;
@@ -24,15 +22,12 @@ public static class ConfigureServices
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         });
         
-        // DbContext
-        service.AddDbContext<MoviesDbContext>(options => options.UseInMemoryDatabase("MoviesDb"));
-        
         // Repositories
         service.AddScoped<IGenderRepository, GenderRepository>();
         service.AddScoped<IGenreRepository, GenreRepository>();
         service.AddScoped<IMovieRepository, MovieRepository>();
         service.AddScoped<IPersonRepository, PersonRepository>();
-        
+
         return service;
     }
 }
